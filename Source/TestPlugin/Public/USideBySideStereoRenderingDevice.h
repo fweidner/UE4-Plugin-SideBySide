72fd0e35af72d66c5883f63239831dbef89caa5d
Florian Weidner <info@florianweidner.de>
2018-05-16 12:13:29 +0200

    Fix: Compile as runtime plugin.

--- Source/TestPlugin/Public/USideBySideStereoRenderingDevice.h
+++ Source/TestPlugin/Public/USideBySideStereoRenderingDevice.h
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-#include "StereoRendering.h"
 #include "RHIStaticStates.h"
+#include "RHIResources.h" /* for FHRICustomPresent */
+#include "StereoRendering.h"
 
 /**
 *
